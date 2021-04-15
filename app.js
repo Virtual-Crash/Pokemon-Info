dccf8692964a05f65f4f6adff0993e129c8b7882
Kayla Jeter <JeterK@spu.edu>
2021-04-14 21:22:00 -0700

    did we break it ?

--- app.js
+++ app.js
@@ -5,7 +5,24 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
-        response.end('Making decisions is hard');
+
+        const req = https.request(options, res => {
+                console.log(`statusCode: ${res.statusCode}`)
+
+                res.on('data', d => {
+                        response.end('Making decisions is hard');
+                        //process.stdout.write(d)
+                })
+        })
+
+        req.on('error', error => {
+                console.error(error)
+        })
+
+        req.end()
+
+
+        
 });
 
 server.listen(port, (err) => {
@@ -14,3 +31,28 @@ server.listen(port, (err) => {
         }
         console.log('Server Listening on: ${port}');
 });
+
+
+
+
+
+const options = {
+        hostname: 'pokeapi.co',
+        port: 443,
+        path: '/api/v2/pokemon/squirtle',
+        method: 'GET'
+}
+
+const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+
+        res.on('data', d => {
+                process.stdout.write(d)
+        })
+})
+
+req.on('error', error => {
+        console.error(error)
+})
+
+req.end()
